a6af43060c65aeb459339e5a8dfbf36eb68f645f
Alan Meachum <alanmmeachum@outlook.com>
2024-04-17 13:40:52 -0400

    Banner/Iframe
    Added Linkedin embeded code.
    
    Added Banner to Blog page.

--- client/src/views/Blog.jsx
+++ client/src/views/Blog.jsx
@@ -18,69 +18,6 @@ const Blog = (props) => {
         "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
   };
-  const posts = [
-    {
-      id: 2,
-      title: "Boost your conversion rate",
-      href: "#",
-      description:
-        "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.",
-      date: "Mar 10, 2020",
-      datetime: "2020-03-16",
-      author: {
-        name: "Lindsay Walton",
-        href: "#",
-        imageUrl:
-          "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-    },
-    {
-      id: 2,
-      title: "Boost your conversion rate",
-      href: "#",
-      description:
-        "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.",
-      date: "Mar 10, 2020",
-      datetime: "2020-03-16",
-      author: {
-        name: "Jane Doe",
-        href: "#",
-        imageUrl:
-          "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-    },
-    {
-      id: 2,
-      title: "Boost your conversion rate",
-      href: "#",
-      description:
-        "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.",
-      date: "Mar 10, 2020",
-      datetime: "2020-03-16",
-      author: {
-        name: "Jane Doe",
-        href: "#",
-        imageUrl:
-          "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-    },
-    {
-      id: 2,
-      title: "Boost your conversion rate",
-      href: "#",
-      description:
-        "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.",
-      date: "Mar 10, 2020",
-      datetime: "2020-03-16",
-      author: {
-        name: "Jane Doe",
-        href: "#",
-        imageUrl:
-          "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-    },
-    // More posts...
-  ];
 
   const { http } = props;
   const [allBlogs, setAllBlogs] = useState([]);
@@ -103,22 +40,48 @@ const Blog = (props) => {
   };
 
   return (
-    //     <div className="blog_main_page">
-    //     {
-    //         allBlogs.map((blog, index) => (
-    //             <div className="blogs" key={blog._id}> {/*Need to set into blocks*/}
-    //                 <h1>{blog.title}</h1>
-    //                 <p className="average_time">Average read time: {calcReadTime(blog.readTime)} minutes</p>
-    //                 <p className="date">{blog.date}</p>
-    //                 {/* <p>{blog.content}</p> */}
-    //             </div>
-    //         ))
-    //     }
-    //     </div>
-    //
+    <div className="pb-10">
+      <div className="relative isolate overflow-hidden pt-40 pb-16 sm:pt-60">
+        <img
+          src="src/assets/keyboard2.png"
+          alt=""
+          className="absolute inset-0 -z-10 object-cover"
+        />
+        <div
+          className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+          aria-hidden="true"
+        >
+          <div
+            className=""
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div
+          className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+          aria-hidden="true"
+        >
+          <div
+            className=""
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-6xl font-bold tracking-tight alan_font_color">
+              Blog
+            </h2>
+            <p className="mt-6 text-m leading-8 text-white italics"></p>
+          </div>
+        </div>
+      </div>
 
-    <div className=" py-24 sm:py-20">
-      <div className="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-12 px-6 sm:gap-y-16 lg:grid-cols-2 lg:px-8">
+      <div className="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-12 px-6 pt-14 sm:gap-y-16 lg:grid-cols-2 lg:px-8">
         <article className="mx-auto w-full max-w-2xl lg:mx-0 lg:max-w-lg">
           <time
             dateTime={featuredPost.datetime}
@@ -145,7 +108,7 @@ const Blog = (props) => {
                 Continue reading <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
-            <div className="flex lg:border-t lg:border-gray-100/10 lg:pt-8">
+            <div className="flex lg:border-t lg:border-gray-100/10 pb-10 lg:pt-8">
               <a
                 href={featuredPost.author.href}
                 className="flex gap-x-2.5 text-sm font-semibold leading-6 text-white"
@@ -159,6 +122,12 @@ const Blog = (props) => {
               </a>
             </div>
           </div>
+          <iframe
+            src="https://widgets.sociablekit.com/linkedin-profile-posts/iframe/25399375"
+            frameborder=""
+            width="100%"
+            height="100%"
+          ></iframe>
         </article>
         <div className="mx-auto w-full max-w-2xl border-t border-gray-900/10 pt-12 sm:pt-16 lg:mx-0 lg:max-w-none lg:border-t-0 lg:pt-0">
           <div className="-my-12 divide-y divide-gray-100/10">
