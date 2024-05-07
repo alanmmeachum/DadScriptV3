3d6fc2161cd1d0fc1069b4dc5278beb8bb0af097
Alan Meachum <alanmmeachum@outlook.com>
2024-05-07 16:28:16 -0400

    Render.io deployment

---
+++ server/client/src/views/Contact.jsx
@@ -0,0 +1,341 @@
+import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/outline";
+import { useNavigate } from "react-router-dom";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const navigate = useNavigate();
+  const form = useRef();
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    emailjs.sendForm("service_meebqd2", "template_a3nqvd8", form.current, {
+      publicKey: "alyv01y3I4mAGs6tJ", //!Concerned about security here since my key is open!
+    });
+    setTimeout(() => {
+      navigate("/blogs");
+    }, 2000);
+  };
+
+  return (
+    <div className="">
+      <div className="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8">
+        <div className="relative bg-white shadow-xl">
+          <h2 className="sr-only">Contact us</h2>
+
+          <div className="grid grid-cols-1 lg:grid-cols-3">
+            {/* Contact information */}
+            <div className="relative overflow-hidden alan_background_color px-6 py-10 sm:px-10 xl:p-12">
+              <div
+                className="pointer-events-none absolute inset-0 sm:hidden"
+                aria-hidden="true"
+              >
+                <svg
+                  className="absolute inset-0 h-full w-full"
+                  width={343}
+                  height={388}
+                  viewBox="0 0 343 388"
+                  fill="none"
+                  preserveAspectRatio="xMidYMid slice"
+                >
+                  <path
+                    d="M-99 461.107L608.107-246l707.103 707.107-707.103 707.103L-99 461.107z"
+                    fill="url(#linear1)"
+                    fillOpacity=".1"
+                  />
+                  <defs>
+                    <linearGradient
+                      id="linear1"
+                      x1="254.553"
+                      y1="107.554"
+                      x2="961.66"
+                      y2="814.66"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stopColor="#fff" />
+                      <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </div>
+              <div
+                className="pointer-events-none absolute bottom-0 right-0 top-0 hidden w-1/2 sm:block lg:hidden"
+                aria-hidden="true"
+              >
+                <svg
+                  className="absolute inset-0 h-full w-full"
+                  width={359}
+                  height={339}
+                  viewBox="0 0 359 339"
+                  fill="none"
+                  preserveAspectRatio="xMidYMid slice"
+                >
+                  <path
+                    d="M-161 382.107L546.107-325l707.103 707.107-707.103 707.103L-161 382.107z"
+                    fill="url(#linear2)"
+                    fillOpacity=".1"
+                  />
+                  <defs>
+                    <linearGradient
+                      id="linear2"
+                      x1="192.553"
+                      y1="28.553"
+                      x2="899.66"
+                      y2="735.66"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stopColor="#fff" />
+                      <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </div>
+              <div
+                className="pointer-events-none absolute bottom-0 right-0 top-0 hidden w-1/2 lg:block"
+                aria-hidden="true"
+              >
+                <svg
+                  className="absolute inset-0 h-full w-full"
+                  width={160}
+                  height={678}
+                  viewBox="0 0 160 678"
+                  fill="none"
+                  preserveAspectRatio="xMidYMid slice"
+                >
+                  <path
+                    d="M-161 679.107L546.107-28l707.103 707.107-707.103 707.103L-161 679.107z"
+                    fill="url(#linear3)"
+                    fillOpacity=".1"
+                  />
+                  <defs>
+                    <linearGradient
+                      id="linear3"
+                      x1="192.553"
+                      y1="325.553"
+                      x2="899.66"
+                      y2="1032.66"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stopColor="#fff" />
+                      <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </div>
+              <h3 className="text-lg font-medium text-white">
+                Contact information
+              </h3>
+              <p className="mt-6 max-w-3xl text-base text-indigo-50">
+                If you are interested in chatting, send a message here or email.
+              </p>
+              <dl className="mt-8 space-y-6">
+                <dt>
+                  <span className="sr-only">Phone number</span>
+                </dt>
+                <dd className="flex text-base text-indigo-50">
+                  <PhoneIcon
+                    className="h-6 w-6 flex-shrink-0 text-indigo-200"
+                    aria-hidden="true"
+                  />
+                  <span className="ml-3 italics">
+                    (Disclosed upon messaging)
+                  </span>
+                </dd>
+                <dt>
+                  <span className="sr-only">Email</span>
+                </dt>
+                <dd className="flex text-base text-indigo-50">
+                  <EnvelopeIcon
+                    className="h-6 w-6 flex-shrink-0 text-indigo-200"
+                    aria-hidden="true"
+                  />
+                  <span className="ml-3">alanmmeachum@gmail.com</span>
+                </dd>
+              </dl>
+              <ul role="list" className="mt-8 flex space-x-12">
+                <li>
+                  <a
+                    className="text-indigo-200 hover:text-indigo-100"
+                    href="https://www.linkedin.com/in/alanmeachum/"
+                  >
+                    <span className="sr-only">Linkedin</span>
+                    <svg
+                      className="h-6 w-6 alan_font_color"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 460 470"
+                    >
+                      <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-indigo-200 hover:text-indigo-100"
+                    href="https://github.com/alanmmeachum"
+                  >
+                    <span className="sr-only">GitHub</span>
+                    <svg
+                      className="h-6 w-6 alan_font_color"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            {/* Contact form */}
+            <div className="px-6 py-10 sm:px-10 lg:col-span-2 xl:p-12">
+              <h3 className="text-lg font-medium text-gray-900">
+                Send me a message
+              </h3>
+              <form
+                onSubmit={onSubmitHandler}
+                ref={form}
+                method="POST"
+                className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+              >
+                <div>
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    First name
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="firstName"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="block w-full rounded-md border-gray-300 px-4 py-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    Last name
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="lastName"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="block w-full rounded-md border-gray-300 px-4 py-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    Email
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      className="block w-full rounded-md border-gray-300 px-4 py-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="flex justify-between">
+                    <label
+                      htmlFor="phone"
+                      className="block text-sm font-medium text-gray-900"
+                    >
+                      Phone
+                    </label>
+                    <span id="phone-optional" className="text-sm text-gray-500">
+                      Optional
+                    </span>
+                  </div>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="phoneNumber"
+                      id="phone"
+                      autoComplete="tel"
+                      className="block w-full rounded-md border-gray-300 px-4 py-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                      aria-describedby="phone-optional"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="subject"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    Subject
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="subject"
+                      id="subject"
+                      className="block w-full rounded-md border-gray-300 px-4 py-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <div className="flex justify-between">
+                    <label
+                      htmlFor="message"
+                      className="block text-sm font-medium text-gray-900"
+                    >
+                      Message
+                    </label>
+                    <span id="message-max" className="text-sm text-gray-500">
+                      Max. 500 characters
+                    </span>
+                  </div>
+                  <div className="mt-1">
+                    <textarea
+                      id="message"
+                      name="message"
+                      rows={4}
+                      className="block w-full rounded-md border-gray-300 px-4 py-3 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                      aria-describedby="message-max"
+                      defaultValue={""}
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2 sm:flex sm:justify-end">
+                  <button
+                    type="submit"
+                    value="Send"
+                    className="mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-green-500 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto "
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
