8e4f15f06505b503e32d504305bca114a3c4a7b0
Alan Meachum <alanmmeachum@outlook.com>
2024-05-13 20:42:12 -0400

    Removed Gradient

--- server/client/src/views/Blog.jsx
+++ server/client/src/views/Blog.jsx
@@ -64,7 +64,7 @@ const Blog = (props) => {
           />
         </div>
         </div>
-<div className="relative px-6 pb-20 lg:px-8 pt-4 lg:pb-28 lg:pt-24 gradient_background">
+<div className="relative px-6 pb-20 lg:px-8 pt-4 lg:pb-28 lg:pt-24">
       <div className="absolute inset-0">
         <div className="h-1/3 sm:h-2/3" />
       </div>
