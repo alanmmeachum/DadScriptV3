592af09aad3e08e704bedb166095f86c24bc7032
Alan Meachum <alanmmeachum@outlook.com>
2024-06-01 23:42:50 -0400

    Added page loader; Modified background color; Removed flashing white when page loads

--- server/client/src/views/Portfolio.jsx
+++ server/client/src/views/Portfolio.jsx
@@ -11,7 +11,7 @@ const Portfolio = () => {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-4">
-            <video className="blog_video" autoPlay muted loop src="/dadscriptblog.mov" type="video/quicktime"/>
+            <video className="blog_video" autoPlay muted loop src="/dadscriptblog.mov" type="video/quicktime" playsInline webkit-playsInline/>
           </div>
           <div>
             <div className="text-base leading-7 text-gray-700 lg:max-w-lg">
